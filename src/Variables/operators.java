29166d04ecfc6a8e38f618f4e82984ece5a8946d
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2026-05-17 19:17:12 +0530

    Operators

---
+++ src/Variables/operators.java
@@ -0,0 +1,40 @@
+package Variables;
+
+public class operators {
+    static void main() {
+        //System.out.println(10%3.4);
+
+//        int a =10;
+//         int b = 10;
+//
+//         int c =  a+++b++;
+//        System.out.println(c);
+//        System.out.println(a);
+//        System.out.println(b);
+//        int d= a+++b;
+//        System.out.println(d);
+//        int x=1010;
+//        System.out.println(x%100);
+//        System.out.println(x/10);
+
+//        int x = 3;
+//        System.out.println(x--);
+//        System.out.println(x);
+
+
+//        int a = 2;
+//        int b = 3;
+//        boolean result = (a++ < b) && (b++ > a);
+//        System.out.println(result);
+//        System.out.println(a);
+//        System.out.println(b);
+
+        int a = 5;
+        int b = a++ + ++a;
+        int c = --a + a++;
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+}
