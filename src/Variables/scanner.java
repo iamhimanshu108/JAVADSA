b74cd97d9733248a412a14808847dac32980c189
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2026-05-17 11:14:49 +0530

    scanner

---
+++ src/Variables/scanner.java
@@ -0,0 +1,24 @@
+package Variables;
+
+import java.util.Scanner;
+
+public class scanner {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println("age = "+ age);
+
+
+        int a =10;
+        int b = 20;
+        System.out.println("before swapping +" +a + " " +b);
+
+        int temp = a;
+        a =b ;
+        b= temp;
+
+        System.out.println("after swapping +" +a + " " +b);
+
+
+    }
+}
