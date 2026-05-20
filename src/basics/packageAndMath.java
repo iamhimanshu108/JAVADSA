ed92644fea22459b81856074a3332b31ce97a238
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2026-05-20 07:07:44 +0530

    Math

---
+++ src/basics/packageAndMath.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class packageAndMath {
+    static void main() {
+        System.out.println(Math.min(10,20));
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.max(55,20));
+        System.out.println(Math.floor(78.52));
+        System.out.println(Math.ceil(78.85));
+        System.out.println(Math.round(10.5));
+        System.out.println(Math.cbrt(5));
+
+
+
+    }
+}
