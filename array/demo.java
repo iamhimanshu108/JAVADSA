e1c20f9df8af26c2bf55a0b61204e7393eb408ea
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2026-08-21 04:58:07 +0000

    Arrays

---
+++ array/demo.java
@@ -0,0 +1,9 @@
+package array;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Hello Arrays");
+    }
+}
+
+
--- src/Demo.java
+++ src/Demo.java
@@ -1,5 +1,5 @@
 public class Demo {
-    static void main() {
+    public static void main(String[] args) {
         System.out.println("Hello");
     }
 }
