57afc2c84ba352ed645f2444c8172f5748fbc3c0
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2026-08-21 10:09:01 +0000

    Postive and Neg

--- array/L27arrayproblems.java
+++ array/L27arrayproblems.java
@@ -101,9 +101,32 @@ public class L27arrayproblems {
 
 
 
+    // Postive and Negative Sum
 
+        static int[] posNegSum(int[] arr){
+            int posSum = 0;
+            int negSum = 0;
 
+            for(int i = 0; i <arr.length; i++){
+                if(arr[i] >0){
+                    posSum = posSum + arr[i];
+                }
+                else {
+                    negSum = negSum + arr[i];
+                }
 
+            }
+            int ans[] = {posSum, negSum};
+            return ans;
+
+        }
+
+        public static void main(String[] args) {
+            int arr[] = {1,2,3,4,5,6,-2,4,6};
+            int ans[] = posNegSum(arr);
+            System.out.println("Postive sum" + ans[0]);
+            System.out.println("Negative sum " + ans[1]);
+        }
 
 
 
