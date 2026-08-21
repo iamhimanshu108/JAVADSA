ef86f4d727ba9c06b157b45c0c746bc26a623406
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2026-08-21 05:43:40 +0000

    Max

---
+++ array/L27arrayproblems.java
@@ -0,0 +1,120 @@
+package array;
+
+public class L27arrayproblems {
+    // static double getAverage(int[] arr){
+    //     double sum = 0;
+        
+    //     for(int i : arr){
+    //         sum += i;
+    //     }
+    //     int size = arr.length;
+    //     double avg = sum/size;
+    //     return avg;
+    // }
+
+    // static int[] mutiplyBy10(int[] arr){
+        
+    //     int size = arr.length;
+    //     int[] newArray = new int[size];
+
+    //     for( int i = 0; i<size; i++){
+    //         int element = arr[i];
+    //         int newElement = element * 10;
+    //         newArray[i] = newElement;
+    //     }
+    //        return newArray;
+        
+    // }
+
+    // public static void main(String[] args){
+    //     int arr[] = {1,2,3,4,5};
+    //     int ans[] = mutiplyBy10(arr);
+    //     System.out.println("Printing Ans");
+    //     for(int i:ans){
+    //         System.out.println(i);
+    //     }
+    // }
+
+    // public static void main(String[] args){
+    //     int[] arr = {1,2,3,4,5};
+    //     //System.out.println(getAverage(arr));
+        
+    // }
+
+
+    //
+
+    // Search for Element in Array linear Search
+
+    // static boolean findTarget(int arr[], int target){
+    //     for(int i =0; i<arr.length; i++){
+    //         if(arr[i] == target){
+    //             return true;
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // public static void main(String[] args) {
+    //     int arr[] = {1,3,4,5,678,89,323};
+    //     boolean ans = findTarget(arr, 12);
+    //     System.out.println(ans);
+
+        
+    // }
+
+
+    // Find The Maximum Element Of An Array
+
+    // static int getMaximum(int[] arr){
+    //     int maxi = arr[0];
+    //      for(int i=0; i <arr.length; i++){
+    //         maxi = Math.max(maxi, arr[i]);
+    //      }
+    //      return maxi;
+        
+    // }
+
+    // public static void main(String[] args) {
+    //     int arr[]  = {1,3,4,6,78,97,0,6};
+    //     System.out.println(getMaximum(arr));
+    // }
+
+
+    // // Find The Maximum Element Of An Array
+
+    // static int getMaximum(int[] arr){
+    //     int maxi = arr[0];
+    //      for(int i=0; i <arr.length; i++){
+    //         if(arr[i] >maxi){
+    //             maxi = arr[i];
+    //         }
+    //      }
+    //      return maxi;
+        
+    // }
+
+    // public static void main(String[] args) {
+    //     int arr[]  = {1,3,4,6,78,97,0,6};
+    //     System.out.println(getMaximum(arr));
+    // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
