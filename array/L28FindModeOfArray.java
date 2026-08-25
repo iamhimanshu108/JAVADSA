19b305b9d6e05dd966bb933428117bb05800ebce
Himanshu Kumar (Yadav) <110648951+iamhimanshu108@users.noreply.github.com>
2026-08-25 11:57:01 +0000

    L28

--- array/L28FindModeOfArray.java
+++ array/L28FindModeOfArray.java
@@ -1,3 +1,26 @@
+import java.util.HashMap;
+
 public class L28FindModeOfArray {
+    static int getMode(int arr[]){
+        HashMap<Integer, Integer> freq =  new HashMap<>();
+        
+        for(int num: arr){
+            freq.put(num, freq.getOrDefault(num, 0)+1);
+
+        }
+
+        int mode = arr[0];
+        for(int num: freq.keySet()){
+            System.out.println(num + "-> " + freq.get(num));
+            if(freq.get(num) > freq.get(mode)){
+                mode = num;
+            }
+        }
+        return mode;
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,3,3,4,5,1,4,5};
+        System.out.println("Mode: " + getMode(arr));
     
+}
 }
