a581ab5401f73e91e61a2e28b0e99599e9fb06cf
Himanshu Kumar (Yadav) <iamhimanshu108@gmail.com>
2026-09-01 06:48:09 +0530

    Add NestedLoop class with main method and nested loops for event printing

---
+++ 17NestedLoop/NestedLoop.java
@@ -0,0 +1,13 @@
+public class NestedLoop {
+    public static void main(String[] args){
+        for(int i =1; i<=5; i++){
+            System.out.println(i+ "th event");
+            for(int j =1; j<=3; j++){
+                System.out.println("Hello World");
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
